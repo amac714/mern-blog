e6c81a9f316adfd9015d06497700136a923a36d7
Alan <amac714@gmail.com>
2019-07-11 17:30:04 -0700

    add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a mern blog that is a work in progress.
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,43 +1,15 @@
 import React, { useState, useReducer } from 'react';
 import { Link } from 'react-router-dom';
-import { loginUser } from '../../actions/authActions';
 import axios from 'axios';
 import setAuthToken from '../../utils/setAuthToken';
-import jwt_decode from 'jwt-decode';
 
-// const isEmpty = require("is-empty");
-
-// const loginReducer = (state, action) => {
-//   switch (action.type) {
-//     case 'SET_CURRENT_USER':
-//       return {
-//         ...state,
-//         isAuthenticated: !isEmpty(action.payload),
-//         user: action.payload
-//       };
-//     case 'USER_LOADING':
-//       return {
-//         ...state,
-//         loading: true
-//       };
-//     case 'GET_ERRORS':
-//       return action.payload;
-//     default:
-//       return state;
-//   }
-// }
 
 function Login(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  // const [state, dispatch] = useReducer(loginReducer, {
-  //   isAuthenticated: false,
-  //   isLoading: false,
-  //   user: {},
-  //   errors: {}
-  // });
-  const userLogin = async userData => {
+
+  const loginUser = async userData => {
     try {
       const result = await axios.post('/api/users/login', userData);
 
@@ -47,20 +19,20 @@ function Login(props) {
 
       // Set token to Auth header
       setAuthToken(token);
-    
-      // Decode token to get user data
-      // const decoded = jwt_decode(token);
+  
+      // redirect to user's dashboard
       props.history.push('/dashboard');
+
     } catch (err) {
       console.log(err);
-      setError('Password or username is incorrect.');
+      setError('Username or password is incorrect.');
     }
   };
 
   const onSubmit = e => {
     e.preventDefault();
     const userData = { username, password };
-    userLogin(userData);
+    loginUser(userData);
   };
 
   return (
