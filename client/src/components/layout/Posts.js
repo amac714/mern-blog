3fda3a6f8503d14ddf61c4c78c3595af868d19f0
Alan <amac714@gmail.com>
2019-09-10 16:18:42 -0700

    added created at date to posts

--- client/src/components/layout/Posts.js
+++ client/src/components/layout/Posts.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 function Posts({ blogPosts, deletePost }) {
 
-  const renderPosts = ({ _id, title, text }) => {
+  const renderPosts = ({ _id, title, text, createdAt }) => {
     return (
       <div className="container" key={_id}>
         <h2>{title}</h2>
         <p>{text}</p>
+        {/* TODO: format createdAt date ex: Tuesday, 9/10/19, 4:20pm */}
+        <p>{createdAt}</p>
         <button className="button" onClick={() => deletePost(`${_id}`)}>
           Delete
         </button>
@@ -14,7 +16,7 @@ function Posts({ blogPosts, deletePost }) {
     );
   };
 
-  return <div>{blogPosts.map(renderPosts)}</div>;
+  return <div>{blogPosts.sort((a,b) => a.createdAt- b.createdAt).map(renderPosts)}</div>;
 };
 
 export default Posts;
