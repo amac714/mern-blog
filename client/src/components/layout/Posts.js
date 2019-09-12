dd7807e723b2f3ad8b36962a89ac889f4aab41ca
Alan <amac714@gmail.com>
2019-09-12 16:32:27 -0700

    fix createdAt date format

--- client/src/components/layout/Posts.js
+++ client/src/components/layout/Posts.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import moment from 'moment';
 
 function Posts({ blogPosts, deletePost }) {
 
   const renderPosts = ({ _id, title, text, createdAt }) => {
+    
     return (
       <div className="container" key={_id}>
         <h2>{title}</h2>
         <p>{text}</p>
         {/* TODO: format createdAt date ex: Tuesday, 9/10/19, 4:20pm */}
-        <p>{createdAt}</p>
+        <p>{moment(createdAt).format('llll')}</p>
         <button className="button" onClick={() => deletePost(`${_id}`)}>
           Delete
         </button>
